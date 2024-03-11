fe5b7c7b44c90c9dd83b7cacb91933d625ed7f29
il-an <150121643+il-an@users.noreply.github.com>
2024-03-11 03:36:24 +0300

    bugfix

--- README.md
+++ README.md
@@ -9,8 +9,3 @@ run `pip3 install -r requirements.txt` to install the dependencies
 ## Usage
 
 run `python3 main.py` to run the program
-
-## Known limitations
-
-DO NOT ENTER CHARACTERS WHERE THEY DO NOT BELONG. THE PROGRAM WILL CRASH.
-If you are asked to input a number, please input a number, not a string.
--- recommendEngine/main.py
+++ recommendEngine/main.py
@@ -5,21 +5,32 @@ import networkx as nx
 import recommendation
 
 n = input("Enter the number of users: ")
+if not n.isdigit():
+    print("Not a number!")
+    quit()
+
+n = int(n)
+if n <= 0:
+    quit()
 
 g = nx.Graph()
 print("Now enter the names of the users")
 # entering the nodes
-for i in range(int(n)):
+for i in range(n):
     user = input("Enter the name: ")
     g.add_node(user)
 
+n = len(g.nodes)
 # entering the edges
-for i in range(int(n)):
-    m = input(f"Enter the number of friends of {list(g.nodes)[i]}: ")
+for i in g.nodes.items():
+    m = input(f"Enter the number of friends of {i[0]}: ")
+    if not m.isdigit():
+        print("Not a number!")
+        quit()
     print("Now enter the names of the friends")
     for j in range(int(m)):
         friend = input("Enter the name: ")
-        g.add_edge(list(g.nodes)[i], friend)
+        g.add_edge(i[0], friend)
 
 print("The graph has been created")
 print("The graph is being displayed...")
@@ -29,9 +40,18 @@ plt.draw()
 plt.show()
 
 # recommending friends
-n = input("How many users do you want to find the friends for:")
+n = input("How many users do you want to find the friends for: ")
+if not n.isdigit():
+    print("Not a number!")
+    quit()
 for i in range(int(n)):
     user = input("Enter the name of the user: ")
+    if user not in g:
+        print("Not in graph!")
+        continue
+    if not recommendation.recommend_by_common_friends(g, user):
+        print("Could not recommend any friends for selected user!")
+        continue
     print("By common friends we can recommend the following friends for you: ", end="")
     cnt = 0
     for friend in recommendation.recommend_by_common_friends(g, user):
